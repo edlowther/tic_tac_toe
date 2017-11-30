8691540106230e80348e8abe88ef2974e11d69be
Ed Lowther <ed.lowther[at]gmail.com>
2017-11-30 16:19:30 +0000

    Add grid concept to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,8 @@
 class Game
-  attr_reader :players
-  def initialize(player_class)
+  attr_reader :players, :grid
+  def initialize(player_class, grid_class)
     @players = []
     2.times { @players << player_class.new }
+    @grid = grid_class.new
   end
 end
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -5,16 +5,29 @@ describe Game do
   let(:player) { double(:player) }
   let(:player_class) { double(:player_class, :new => player)}
 
+  let(:grid) { double(:grid) }
+  let(:grid_class) { double(:grid_class, :new => grid)}
+
   describe '#new' do
 
     it 'creates two new players' do
-      game = described_class.new(player_class)
+      game = described_class.new(player_class, grid_class)
       expect(player_class).to have_received(:new).twice
     end
 
     it 'adds the two players to an internal array' do
-      game = described_class.new(player_class)
+      game = described_class.new(player_class, grid_class)
       expect(game.players).to eq [player, player]
     end
+
+    it 'creates a new grid' do
+      game = described_class.new(player_class, grid_class)
+      expect(grid_class).to have_received(:new)
+    end
+
+    it 'saves the new grid as an instance variable' do
+      game = described_class.new(player_class, grid_class)
+      expect(game.grid).to eq grid
+    end 
   end
 end
