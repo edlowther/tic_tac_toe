eac452aa40abf5578aeb81faf983c157a94068d9
Ed Lowther <ed.lowther[at]gmail.com>
2017-11-30 16:19:50 +0000

    Begin work on Player class

---
+++ lib/player.rb
@@ -0,0 +1,13 @@
+class Player
+  @@player_count = 0
+  attr_reader :id, :occupied_squares
+  def initialize
+    @@player_count += 1
+    @id = @@player_count == 1 ? "O" : "X"
+    @occupied_squares = []
+  end
+
+  def add_square(square)
+    @occupied_squares << square
+  end
+end
---
+++ spec/units/player_spec.rb
@@ -0,0 +1,23 @@
+require 'player'
+
+describe Player do
+  subject(:player) { described_class.new }
+  let(:square) { double(:square) }
+  describe '#new' do
+    player1 = Player.new
+    player2 = Player.new
+    it 'first player is assigned an id of "O"' do
+      expect(player1.id).to eq "O"
+    end
+    it 'second player is assigned an id of "X"' do
+      expect(player2.id).to eq "X"
+    end
+  end
+
+  describe '#add_square' do
+    it 'pushes a square object to the instance\'s occupied_squares array' do
+      player.add_square(square)
+      expect(player.occupied_squares.first).to eq square
+    end
+  end
+end
